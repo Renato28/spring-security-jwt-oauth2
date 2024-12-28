9b84ec9cc1b536d68da13b4e8eca87881110c796
Renato28 <renatonobrega87@gmail.com>
2024-12-28 14:28:19 -0300

    implementando a classe Tweet

---
+++ src/main/java/io/github/renatonobrega/springsecurity/entities/Tweet.java
@@ -0,0 +1,56 @@
+package io.github.renatonobrega.springsecurity.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String content;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Instant creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
+}
