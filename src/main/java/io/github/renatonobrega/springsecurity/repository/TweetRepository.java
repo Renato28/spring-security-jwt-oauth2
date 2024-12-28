6338c6ff4aeb12516af0c69f63e93440f717fe5c
Renato28 <renatonobrega87@gmail.com>
2024-12-28 14:36:52 -0300

    implementando a interface TweetRepository

---
+++ src/main/java/io/github/renatonobrega/springsecurity/repository/TweetRepository.java
@@ -0,0 +1,10 @@
+package io.github.renatonobrega.springsecurity.repository;
+
+import io.github.renatonobrega.springsecurity.entities.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+    
+}
