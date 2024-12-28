ff22641e7ae8aea5304b9d65a50f71f9cea4adbf
Renato28 <renatonobrega87@gmail.com>
2024-12-28 14:34:41 -0300

    implementando a interface RoleRepository

--- src/main/java/io/github/renatonobrega/springsecurity/repository/UserRepository.java
+++ src/main/java/io/github/renatonobrega/springsecurity/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package io.github.renatonobrega.springsecurity;
+package io.github.renatonobrega.springsecurity.repository;
 
 import io.github.renatonobrega.springsecurity.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
